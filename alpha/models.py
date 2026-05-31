020c3591691710d682cd881d9dcd5122f2451390
Surendhar <surendhar3538@gmail.com>
2026-05-31 18:54:46 +0530

    Fix NameError by importing base64 library in models.py

--- alpha/models.py
+++ alpha/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
+import base64
 
 class Category(models.Model):
     name=models.CharField(max_length=100)
