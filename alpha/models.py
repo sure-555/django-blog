f889437d94ff03666f31c5a0fac57b1c943bf6f8
Surendhar <surendhar3538@gmail.com>
2026-05-31 18:11:23 +0530

    Move image_base64 property out of views and into models

--- alpha/models.py
+++ alpha/models.py
@@ -9,30 +9,26 @@ class Category(models.Model):
         return self.name
 
 class Post(models.Model):
-    title=models.CharField(max_length=100)
-    content=models.TextField()
-    image_data=models.BinaryField(null=True, blank=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.CharField(max_length=500, null=True, blank=True)
+    image_data = models.BinaryField(null=True, blank=True) 
     image_mime_type = models.CharField(max_length=50, null=True, blank=True)
-    created_at=models.DateField(auto_now_add=True)
-    slug=models.SlugField(unique=True)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    is_piublished=models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=200, unique=True)
+    is_piublished = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        self.slug= slugify(self.title)
-        super().save(*args, **kwargs)
-
+    # ---> PASTE IT EXACTLY HERE, INSIDE THE MODEL CLASS <---
     @property
-    def formatted_img_url(self):
-        if self.image.__str__().startswith(('http://','https://')):
-            url=self.image
-        else:
-            url=self.image.url
-        return url
-
-    def __str__(self):
-        return self.title
+    def image_base64(self):
+        if self.image_data and self.image_mime_type:
+            binary_as_string = base64.b64encode(self.image_data).decode('utf-8')
+            return f"data:{self.image_mime_type};base64,{binary_as_string}"
+        elif self.image:
+            return self.image
+        return None 
 
 
 class AboutUs(models.Model):
